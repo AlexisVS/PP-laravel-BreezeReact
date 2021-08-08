59d6d297bac360053ae2f2c0c99f85428238d7e4
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-08-08 11:02:52 +0200

    base

---
+++ resources/components/Application.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import React from 'react';
+import { render } from 'react-dom';
+
+const Application = () => {
+  return (
+    <div>
+      <h1>Bonjour J'ai réussi a installer reactJS sur une application PHP Laravel</h1>
+    </div>
+  );
+};
+
+export default Application;
+
+const mainApp = document.getElementById('mainApp');
